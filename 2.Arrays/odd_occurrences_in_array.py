968e8b1a929579b0b8b670c1c2f18eebff026d5f
Victoria <victoria.sofianidou@gmail.com>
2020-12-16 12:25:07 +0100

    found odd occurrence in array using bitwise operators XOR

--- 2.Arrays/odd_occurrences_in_array.py
+++ 2.Arrays/odd_occurrences_in_array.py
@@ -1,10 +1,11 @@
 """
-A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+A non-empty array A consisting of N integers is given. 
+The array contains an odd number of elements, 
+and each element of the array can be paired with another element that has the same value, 
+except for one element that is left unpaired.
 
 For example, in array A such that:
-  A[0] = 9  A[1] = 3  A[2] = 9
-  A[3] = 3  A[4] = 9  A[5] = 7
-  A[6] = 9
+  A[0] = 9  A[1] = 3  A[2] = 9  A[3] = 3  A[4] = 9  A[5] = 7 A[6] = 9
 
         the elements at indexes 0 and 2 have value 9,
         the elements at indexes 1 and 3 have value 3,
@@ -13,14 +14,13 @@ For example, in array A such that:
 
 Write a function:
 
-    class Solution { public int solution(int[] A); }
+    def solution(A)
 
-that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+that, given an array A consisting of N integers fulfilling the above conditions, 
+returns the value of the unpaired element.
 
 For example, given array A such that:
-  A[0] = 9  A[1] = 3  A[2] = 9
-  A[3] = 3  A[4] = 9  A[5] = 7
-  A[6] = 9
+  A[0] = 9  A[1] = 3  A[2] = 9  A[3] = 3  A[4] = 9  A[5] = 7  A[6] = 9
 
 the function should return 7, as explained in the example above.
 
@@ -30,3 +30,18 @@ Write an efficient algorithm for the following assumptions:
         each element of array A is an integer within the range [1..1,000,000,000];
         all but one of the values in A occur an even number of times.
 """
+
+def solution(A):
+
+  x=0
+  for number in A:
+    x ^= number
+
+  return x
+
+A = [9, 3, 9, 3, 9, 7, 9]    
+print(solution(A))
+
+#https://www.techiedelight.com/find-odd-occurring-element-array-single-traversal/
+#https://www.tutorialspoint.com/python/bitwise_operators_example.htm
+# ^ Binary XOR: It copies the bit if it is set in one operand but not both.
