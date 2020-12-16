8ee74d438d8c8a6b256e3179c8c868adb05520e6
Victoria <victoria.sofianidou@gmail.com>
2020-12-16 18:01:06 +0100

    challenging to understand and solve tape equilibrium

--- 3.Time Complexity/tape_equilibrium.py
+++ 3.Time Complexity/tape_equilibrium.py
@@ -1,44 +1,52 @@
 """
 A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
 
-Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: 
+...
 
-The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
-
-In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+The difference between the two parts is ...
+the absolute difference between the sum of the first part and the sum of the second part.
 
 For example, consider array A such that:
-  A[0] = 3
-  A[1] = 1
-  A[2] = 2
-  A[3] = 4
-  A[4] = 3
+A = [3, 1, 2, 4, 3]
 
 We can split this tape in four places:
-
-        P = 1, difference = |3 − 10| = 7
-        P = 2, difference = |4 − 9| = 5
-        P = 3, difference = |6 − 7| = 1
-        P = 4, difference = |10 − 3| = 7
+...
 
 Write a function:
 
     def solution(A)
 
-that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+that, given a non-empty array A of N integers, 
+returns the minimal difference that can be achieved.
 
 For example, given:
-  A[0] = 3
-  A[1] = 1
-  A[2] = 2
-  A[3] = 4
-  A[4] = 3
+A = [3, 1, 2, 4, 3]
 
 the function should return 1, as explained above.
 
 Write an efficient algorithm for the following assumptions:
 
         N is an integer within the range [2..100,000];
-        each element of array A is an integer within the range [−1,000..1,000].
 
 """
+
+def solution(A):  
+
+    left_sum = 0
+    array_sum = sum(A)
+    minval = float('inf')
+    
+    for i in A[:-1]:
+        left_sum += i
+        minval = min(abs(array_sum - 2*left_sum), minval)
+    
+    return minval
+
+A = [3, 1, 2, 4, 3]
+print(solution(A))    
+
+#https://stackoverflow.com/questions/48030130/codility-tape-equilibrium-training-using-python
+#https://stackoverflow.com/questions/34264710/what-is-the-point-of-floatinf-in-python
+#float('inf') acts as an unbounded upper value for comparison. 
+#This is useful for finding lowest values for something. 
