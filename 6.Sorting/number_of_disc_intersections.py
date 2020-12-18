80233225419b4a0df64f074438da81a0140dda3b
Victoria <victoria.sofianidou@gmail.com>
2020-12-18 11:57:43 +0100

    difficult exercise, explained well by youtube Python Learning

--- 6.Sorting/number_of_disc_intersections.py
+++ 6.Sorting/number_of_disc_intersections.py
@@ -1,7 +1,10 @@
 """
-We draw N discs on a plane. The discs are numbered from 0 to N − 1. An array A of N non-negative integers, specifying the radiuses of the discs, is given. The J-th disc is drawn with its center at (J, 0) and radius A[J].
+We draw N discs on a plane. ...
+An array A of N non-negative integers, specifying the radiuses of the discs, is given. 
+The J-th disc is drawn with its center at (J, 0) and radius A[J].
 
-We say that the J-th disc and K-th disc intersect if J ≠ K and the J-th and K-th discs have at least one common point (assuming that the discs contain their borders).
+...
+have at least one common point (assuming that the discs contain their borders).
 
 The figure below shows discs drawn for N = 6 and A as follows:
   A[0] = 1
@@ -20,7 +23,9 @@ Write a function:
 
     def solution(A)
 
-that, given an array A describing N discs as explained above, returns the number of (unordered) pairs of intersecting discs. The function should return −1 if the number of intersecting pairs exceeds 10,000,000.
+that, given an array A describing N discs as explained above, 
+returns the number of (unordered) pairs of intersecting discs. 
+...
 
 Given array A shown above, the function should return 11, as explained above.
 
@@ -30,3 +35,71 @@ Write an efficient algorithm for the following assumptions:
         each element of array A is an integer within the range [0..2,147,483,647].
 
 """
+
+#stackoverflow solution:
+"""
+from bisect import bisect_right
+
+def solution(A):
+    pairs = 0
+
+    # create an array of tuples, each containing the start and end indices of a disk
+    # some indices may be less than 0 or greater than len(A), this is fine!
+    # sort the array by the first entry of each tuple: the disk start indices
+    intervals = sorted( [(i-A[i], i+A[i]) for i in range(len(A))] )
+
+    # create an array of starting indices using tuples in intervals
+    starts = [i[0] for i in intervals]
+
+    # for each disk in order of the *starting* position of the disk, not the centre
+    for i in range(len(starts)):
+
+        # find the end position of that disk from the array of tuples
+        disk_end = intervals[i][1]
+
+        # find the index of the rightmost value less than or equal to the interval-end
+        # this finds the number of disks that have started before disk i ends
+        count = bisect_right(starts, disk_end )
+
+        # subtract current position to exclude previous matches
+        # this bit seemed 'magic' to me, so I think of it like this...
+        # for disk i, i disks that start to the left have already been dealt with
+        # subtract i from count to prevent double counting
+        # subtract one more to prevent counting the disk itsself
+        count -= (i+1)
+        pairs += count
+        if pairs > 10000000:
+            return -1
+    return pairs
+"""
+#more understandable solution for me: 
+
+from bisect import bisect_right
+#This module provides support for maintaining a list in sorted order 
+# without having to sort the list after each insertion.
+
+def solution(A):
+        start = [i-j for i, j in enumerate(A)]
+        #use enumarate() to have a variable that changes on each loop iteration. 
+
+        start.sort()
+        pairs = 0
+
+        for i in range(0, len(A)):
+
+                end = i + A[i]
+                count = bisect_right(start, end)
+
+                count_1 = count-1
+                count_2 = count_1-i
+
+                pairs += count_2        
+
+        return pairs
+
+
+A = [1, 5, 2, 1, 4, 0]
+print(solution(A))
+
+#https://stackoverflow.com/questions/4801242/algorithm-to-calculate-number-of-intersecting-discs
+#https://www.youtube.com/watch?v=NYjnoZulqrQ&t=61s
