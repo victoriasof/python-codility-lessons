44d4f70a07a423e7688ed6ca97a9bfd7f60afb5d
Victoria <victoria.sofianidou@gmail.com>
2020-12-18 10:38:04 +0100

    found max product of three by sorting array, splitting in two sections and returning max of either sections

--- 6.Sorting/max_product_of_three.py
+++ 6.Sorting/max_product_of_three.py
@@ -1,5 +1,6 @@
 """
-A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+A non-empty array A consisting of N integers is given. 
+...
 
 For example, array A such that:
   A[0] = -3
@@ -11,7 +12,7 @@ For example, array A such that:
 
 contains the following example triplets:
 
-        (0, 1, 2), product is −3 * 1 * 2 = −6
+        ...
         (1, 2, 4), product is 1 * 2 * 5 = 10
         (2, 4, 5), product is 2 * 5 * 6 = 60
 
@@ -35,8 +36,27 @@ the function should return 60, as the product of triplet (2, 4, 5) is maximal.
 
 Write an efficient algorithm for the following assumptions:
 
-        N is an integer within the range [3..100,000];
-        each element of array A is an integer within the range [−1,000..1,000].
+    ...
 
 """
+
+def solution(A):
+    
+  #A = sorted(A)
+  A.sort()
+
+  return max(A[-3]*A[-2]*A[-1] , A[0]*A[1]*A[-1])
+  #split array into two sections and return max of either section 
+  #If an index has a negative value, it counts backward from the end of the list
+  #numbers[-1] is the last element of the list, numbers[-2] is the second to last...
+  #remember that two negatives equal positive
+
+A = [-3, 1, 2, -2, 5, 6]
+print(solution(A))
+
+#https://codesays.com/2014/solution-to-max-product-of-three-by-codility/
+#https://www.youtube.com/watch?v=vslRM80lvrY
+
+
+
 
