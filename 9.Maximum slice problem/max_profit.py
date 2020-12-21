c2c843d20ffdb66a263f005267804ef8a8cbea00
Victoria <victoria.sofianidou@gmail.com>
2020-12-21 16:25:38 +0100

    solution from codesays

--- 9.Maximum slice problem/max_profit.py
+++ 9.Maximum slice problem/max_profit.py
@@ -1,5 +1,7 @@
 """
-An array A consisting of N integers is given. It contains daily prices of a stock share for a period of N consecutive days. If a single share was bought on day P and sold on day Q, where 0 ≤ P ≤ Q < N, then the profit of such transaction is equal to A[Q] − A[P], provided that A[Q] ≥ A[P]. Otherwise, the transaction brings loss of A[P] − A[Q].
+An array A consisting of N integers is given. 
+It contains daily prices of a stock share for a period of N consecutive days. 
+...
 
 For example, consider the following array A consisting of six elements such that:
   A[0] = 23171
@@ -9,13 +11,23 @@ For example, consider the following array A consisting of six elements such that
   A[4] = 21013
   A[5] = 21367
 
-If a share was bought on day 0 and sold on day 2, a loss of 2048 would occur because A[2] − A[0] = 21123 − 23171 = −2048. If a share was bought on day 4 and sold on day 5, a profit of 354 would occur because A[5] − A[4] = 21367 − 21013 = 354. Maximum possible profit was 356. It would occur if a share was bought on day 1 and sold on day 5.
+If a share was bought on day 0 and sold on day 2, 
+...
+
+If a share was bought on day 4 and sold on day 5, 
+...
+
+Maximum possible profit was 356. 
+It would occur if a share was bought on day 1 and sold on day 5.
 
 Write a function,
 
     def solution(A)
 
-that, given an array A consisting of N integers containing daily prices of a stock share for a period of N consecutive days, returns the maximum possible profit from one transaction during this period. The function should return 0 if it was impossible to gain any profit.
+that, given an array A consisting of N integers containing 
+daily prices of a stock share for a period of N consecutive days, 
+returns the maximum possible profit from one transaction during this period. 
+The function should return 0 if it was impossible to gain any profit.
 
 For example, given array A consisting of six elements such that:
   A[0] = 23171
@@ -33,3 +45,23 @@ Write an efficient algorithm for the following assumptions:
         each element of array A is an integer within the range [0..200,000].
 
 """
+
+import sys
+
+def solution(A):
+
+  min_price = sys.maxsize
+  max_profit = 0
+
+  for i in A:
+    min_price = min([min_price, i])
+    max_profit = max([max_profit, i - min_price])
+
+  return max_profit
+
+
+A = [23171, 21011, 21123, 21366, 21013, 21367]
+print(solution(A))
+
+#https://www.geeksforgeeks.org/sys-maxsize-in-python/
+
--- 9.Maximum slice problem/max_slice_sum.py
+++ 9.Maximum slice problem/max_slice_sum.py
@@ -1,11 +1,13 @@
 """
-A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P ≤ Q < N, is called a slice of array A. The sum of a slice (P, Q) is the total of A[P] + A[P+1] + ... + A[Q].
+A non-empty array A consisting of N integers is given. 
+...
 
 Write a function:
 
     def solution(A)
 
-that, given an array A consisting of N integers, returns the maximum sum of any slice of A.
+that, given an array A consisting of N integers, 
+returns the maximum sum of any slice of A.
 
 For example, given array A such that:
 A[0] = 3  A[1] = 2  A[2] = -6
@@ -14,14 +16,29 @@ A[3] = 4  A[4] = 0
 the function should return 5 because:
 
         (3, 4) is a slice of A that has sum 4,
-        (2, 2) is a slice of A that has sum −6,
-        (0, 1) is a slice of A that has sum 5,
+        ...
         no other slice of A has sum greater than (0, 1).
 
 Write an efficient algorithm for the following assumptions:
 
         N is an integer within the range [1..1,000,000];
-        each element of array A is an integer within the range [−1,000,000..1,000,000];
-        the result will be an integer within the range [−2,147,483,648..2,147,483,647].
+        ...
 
 """
+
+def solution(A):
+
+        max_slice = 0
+        result = A[0]
+
+        for value in A:
+                max_slice = max(value, max_slice + value)
+                result = max(result, max_slice)
+
+        return result 
+
+
+A = [3, 2, -6, 4, 0]
+print(solution(A))                
+
+#https://codesays.com/2014/solution-to-max-slice-sum-by-codility/
