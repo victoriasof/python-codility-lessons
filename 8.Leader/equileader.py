7aac823fd912a95762ad2c0ceffb3e84b4d86200
Victoria <victoria.sofianidou@gmail.com>
2020-12-21 12:29:17 +0100

    solution from codesays

--- 8.Leader/equileader.py
+++ 8.Leader/equileader.py
@@ -3,8 +3,7 @@ A non-empty array A consisting of N integers is given.
 
 The leader of this array is the value that occurs in more than half of the elements of A.
 
-An equi leader is an index S such that 0 ≤ S < N − 1 and two sequences A[0], A[1], ..., A[S] 
-and A[S + 1], A[S + 2], ..., A[N − 1] have leaders of the same value.
+...
 
 For example, given array A such that:
     A[0] = 4
@@ -39,7 +38,63 @@ the function should return 2, as explained above.
 
 Write an efficient algorithm for the following assumptions:
 
-        N is an integer within the range [1..100,000];
-        each element of array A is an integer within the range [−1,000,000,000..1,000,000,000].
+        ...
+"""
 
 """
+#WRONG SOLUTION ??
+
+from collections import Counter
+
+def solution(A):
+
+    result = 0
+    equileader, equil_count = Counter(A).most_common()[0]
+
+    left = 0
+
+    for i, value in enumerate(A):
+
+        if value == equileader:
+            left += 1
+
+        if left > (i+1) // 2 and (equil_count - left) > (len(A) - (i + 1)) // 2: 
+            result +=1
+
+        return result         
+"""        
+
+def solution(A):
+
+    size = len(A)
+    count, count1, sequence, answer = 0, 0, 0, 0
+
+    for i in A:
+        if 0 == count:
+            sequence = i
+
+        if sequence == i:
+            count += 1
+
+        else:
+            count -= 1
+
+    count = A.count(sequence)
+
+    if count > size // 2:
+
+        for i in xrange(size):
+
+            if A[i] == sequence:
+                count1 += 1
+
+            if count1 > (i + 1) // 2 and count - count1 > (size - 1 - i) // 2:
+                answer += 1
+
+    return answer
+
+
+A = [4, 3, 4, 4, 4, 2]
+print(solution(A))
+
+#https://codesays.com/2014/solution-to-equi-leader-by-codility/
