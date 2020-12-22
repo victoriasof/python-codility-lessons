01997f0632a0c142f71b7c5ab5270877d3087813
Victoria <victoria.sofianidou@gmail.com>
2020-12-22 14:30:59 +0100

    getting spicy

--- Lesson 11.Sieve of Eratosthenes/count_non_divisible.py
+++ Lesson 11.Sieve of Eratosthenes/count_non_divisible.py
@@ -2,7 +2,8 @@
 
 You are given an array A consisting of N integers.
 
-For each number A[i] such that 0 ≤ i < N, we want to count the number of elements of the array that are not the divisors of A[i]. We say that these elements are non-divisors.
+...
+We say that these elements are non-divisors.
 
 For example, consider integer N = 5 and array A such that:
     A[0] = 3
@@ -23,7 +24,8 @@ Write a function:
 
     def solution(A)
 
-that, given an array A consisting of N integers, returns a sequence of integers representing the amount of non-divisors.
+that, given an array A consisting of N integers, 
+returns a sequence of integers representing the amount of non-divisors.
 
 Result array should be returned as an array of integers.
 
@@ -42,3 +44,42 @@ Write an efficient algorithm for the following assumptions:
         each element of array A is an integer within the range [1..2 * N].
 
 """
+
+def solution(A):
+  
+    A_max = max(A)
+  
+    count = {}
+    for element in A:
+        if element not in count:
+            count[element] = 1
+        else:
+            count[element] += 1
+  
+    divisors = {}
+    for element in A:
+        divisors[element] = set([1, element])
+  
+    # start the Sieve of Eratosthenes
+    divisor = 2
+    while divisor*divisor <= A_max:
+        element_candidate = divisor
+
+        while element_candidate  <= A_max:
+            if element_candidate in divisors and not divisor in divisors[element_candidate]:
+                divisors[element_candidate].add(divisor)
+                divisors[element_candidate].add(element_candidate//divisor)
+            element_candidate += divisor
+        divisor += 1
+  
+    result = [0] * len(A)
+    for idx, element in enumerate(A):
+        result[idx] = (len(A)-sum([count.get(divisor,0) for divisor in divisors[element]]))
+  
+    return result
+
+
+A = [3, 1, 2, 3, 6]
+print(solution(A))
+
+#https://www.martinkysel.com/codility-countnondivisible-solution/
