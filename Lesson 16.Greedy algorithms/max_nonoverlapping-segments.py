3daecb0208625c91d91acf77c238cad2b2ef88f5
Victoria <victoria.sofianidou@gmail.com>
2020-12-24 09:43:13 +0100

    solution from martinkysel

--- Lesson 16.Greedy algorithms/max_nonoverlapping-segments.py
+++ Lesson 16.Greedy algorithms/max_nonoverlapping-segments.py
@@ -1,11 +1,16 @@
 """
 
+Located on a line are N segments, numbered from 0 to N - 1, 
+whose positions are given in arrays A and B. 
+For each I (0 <= I < N) the position of segment I is from A[I] to B[I] (inclusive). 
+The segments are sorted by their ends, which means that B[K] <= B[K + 1] for K 
+such that 0 <= K < N - 1.
 
-Located on a line are N segments, numbered from 0 to N − 1, whose positions are given in arrays A and B. For each I (0 ≤ I < N) the position of segment I is from A[I] to B[I] (inclusive). The segments are sorted by their ends, which means that B[K] ≤ B[K + 1] for K such that 0 ≤ K < N − 1.
+Two segments I and J, such that ... , are overlapping if they share at least one common point. 
+In other words, A[I] <= A[J] <= B[I] or A[J] <= A[I] <= B[J].
 
-Two segments I and J, such that I ≠ J, are overlapping if they share at least one common point. In other words, A[I] ≤ A[J] ≤ B[I] or A[J] ≤ A[I] ≤ B[J].
-
-We say that the set of segments is non-overlapping if it contains no two overlapping segments. The goal is to find the size of a non-overlapping set containing the maximal number of segments.
+We say that the set of segments is non-overlapping if it contains no two overlapping segments. 
+The goal is to find the size of a non-overlapping set containing the maximal number of segments.
 
 For example, consider arrays A, B such that:
     A[0] = 1    B[0] = 5
@@ -16,13 +21,16 @@ For example, consider arrays A, B such that:
 
 The segments are shown in the figure below.
 
-The size of a non-overlapping set containing a maximal number of segments is 3. For example, possible sets are {0, 2, 3}, {0, 2, 4}, {1, 2, 3} or {1, 2, 4}. There is no non-overlapping set with four segments.
+The size of a non-overlapping set containing a maximal number of segments is 3. 
+For example, possible sets are {0, 2, 3}, {0, 2, 4}, {1, 2, 3} or {1, 2, 4}. 
+There is no non-overlapping set with four segments.
 
 Write a function:
 
     def solution(A, B)
 
-that, given two arrays A and B consisting of N integers, returns the size of a non-overlapping set containing a maximal number of segments.
+that, given two arrays A and B consisting of N integers, 
+returns the size of a non-overlapping set containing a maximal number of segments.
 
 For example, given arrays A, B shown above, the function should return 3, as explained above.
 
@@ -30,7 +38,31 @@ Write an efficient algorithm for the following assumptions:
 
         N is an integer within the range [0..30,000];
         each element of arrays A, B is an integer within the range [0..1,000,000,000];
-        A[I] ≤ B[I], for each I (0 ≤ I < N);
-        B[K] ≤ B[K + 1], for each K (0 ≤ K < N − 1).
+        A[I] <= B[I], for each I (0 <= I < N);
+        B[K] <= B[K + 1], for each K (0 <= K < N - 1).
 
 """
+
+def solution(A, B):
+
+    if len(A) < 1:
+        return 0
+     
+    count = 1
+    prev_end = B[0]
+     
+    for i in xrange(1, len(A)):
+
+        if A[i] > prev_end:
+            count += 1
+            prev_end = B[i]
+     
+    return count
+
+A = [1, 3, 7, 9, 9]
+B = [5, 6, 8, 9, 10]
+
+print(solution(A, B))  
+
+#https://www.martinkysel.com/codility-max-nonoverlapping-segments-solution/
+
