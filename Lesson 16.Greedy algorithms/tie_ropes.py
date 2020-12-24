bb24e9f9aac68ad8fe63b88c9e993f2403026a33
Victoria <victoria.sofianidou@gmail.com>
2020-12-24 10:19:08 +0100

    solution from martinkysel

--- Lesson 16.Greedy algorithms/tie_ropes.py
+++ Lesson 16.Greedy algorithms/tie_ropes.py
@@ -1,9 +1,15 @@
 """
-There are N ropes numbered from 0 to N − 1, whose lengths are given in an array A, lying on the floor in a line. For each I (0 ≤ I < N), the length of rope I on the line is A[I].
+There are N ropes numbered from 0 to N - 1, whose lengths are given in an array A, 
+lying on the floor in a line. 
+For each I (0 <= I < N), the length of rope I on the line is A[I].
 
-We say that two ropes I and I + 1 are adjacent. Two adjacent ropes can be tied together with a knot, and the length of the tied rope is the sum of lengths of both ropes. The resulting new rope can then be tied again.
+We say that two ropes I and I + 1 are adjacent. 
+Two adjacent ropes can be tied together with a knot, 
+and the length of the tied rope is the sum of lengths of both ropes. 
+The resulting new rope can then be tied again.
 
-For a given integer K, the goal is to tie the ropes in such a way that the number of ropes whose length is greater than or equal to K is maximal.
+For a given integer K, the goal is to tie the ropes in such a way 
+that the number of ropes whose length is greater than or equal to K is maximal.
 
 For example, consider K = 4 and array A such that:
     A[0] = 1
@@ -21,13 +27,15 @@ We can tie:
         rope 1 with rope 2 to produce a rope of length A[1] + A[2] = 5;
         rope 4 with rope 5 with rope 6 to produce a rope of length A[4] + A[5] + A[6] = 5.
 
-After that, there will be three ropes whose lengths are greater than or equal to K = 4. It is not possible to produce four such ropes.
+After that, there will be three ropes whose lengths are greater than or equal to K = 4. 
+It is not possible to produce four such ropes.
 
 Write a function:
 
     def solution(K, A)
 
-that, given an integer K and a non-empty array A of N integers, returns the maximum number of ropes of length greater than or equal to K that can be created.
+that, given an integer K and a non-empty array A of N integers, 
+returns the maximum number of ropes of length greater than or equal to K that can be created.
 
 For example, given K = 4 and array A such that:
     A[0] = 1
@@ -47,3 +55,27 @@ Write an efficient algorithm for the following assumptions:
         each element of array A is an integer within the range [1..1,000,000,000].
 
 """
+
+def solution(K, A):
+
+
+    count = 0
+    rope_length = 0
+
+    for i in A:
+        rope_length += i
+
+        if rope_length >= K:
+            count +=1
+            rope_length = 0
+ 
+    return count
+
+
+K = 4
+A = [1, 2, 3, 4, 1, 1, 3]
+
+print(solution(K,A))
+
+#https://www.martinkysel.com/codility-tieropes-solution/
+
