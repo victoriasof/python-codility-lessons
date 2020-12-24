28d1826726aefc4a42cd170df9e62746498dae2f
Victoria <victoria.sofianidou@gmail.com>
2020-12-24 12:20:41 +0100

    codesays solution

--- Lesson 17.Dynamic programming/number_solitair.py
+++ Lesson 17.Dynamic programming/number_solitair.py
@@ -1,11 +1,22 @@
 """
-A game for one player is played on a board consisting of N consecutive squares, numbered from 0 to N − 1. There is a number written on each square. A non-empty array A of N integers contains the numbers written on the squares. Moreover, some squares can be marked during the game.
+A game for one player is played on a board consisting of N consecutive squares, 
+numbered from 0 to N - 1. There is a number written on each square. 
+A non-empty array A of N integers contains the numbers written on the squares. 
+Moreover, some squares can be marked during the game.
 
-At the beginning of the game, there is a pebble on square number 0 and this is the only square on the board which is marked. The goal of the game is to move the pebble to square number N − 1.
+At the beginning of the game, there is a pebble on square number 0 
+and this is the only square on the board which is marked. 
+The goal of the game is to move the pebble to square number N - 1.
 
-During each turn we throw a six-sided die, with numbers from 1 to 6 on its faces, and consider the number K, which shows on the upper face after the die comes to rest. Then we move the pebble standing on square number I to square number I + K, providing that square number I + K exists. If square number I + K does not exist, we throw the die again until we obtain a valid move. Finally, we mark square number I + K.
+During each turn we throw a six-sided die, with numbers from 1 to 6 on its faces, 
+and consider the number K, which shows on the upper face after the die comes to rest. 
+Then we move the pebble standing on square number I to square number I + K, 
+providing that square number I + K exists. If square number I + K does not exist, 
+we throw the die again until we obtain a valid move. Finally, we mark square number I + K.
 
-After the game finishes (when the pebble is standing on square number N − 1), we calculate the result. The result of the game is the sum of the numbers written on all marked squares.
+After the game finishes (when the pebble is standing on square number N - 1), 
+we calculate the result. The result of the game is the sum of the numbers 
+written on all marked squares.
 
 For example, given the following array:
     A[0] = 1
@@ -18,17 +29,20 @@ For example, given the following array:
 one possible game could be as follows:
 
         the pebble is on square number 0, which is marked;
-        we throw 3; the pebble moves from square number 0 to square number 3; we mark square number 3;
+        we throw 3; the pebble moves from square number 0 to square number 3;
+         we mark square number 3;
         we throw 5; the pebble does not move, since there is no square number 8 on the board;
         we throw 2; the pebble moves to square number 5; we mark this square and the game ends.
 
-The marked squares are 0, 3 and 5, so the result of the game is 1 + 9 + (−2) = 8. This is the maximal possible result that can be achieved on this board.
+The marked squares are 0, 3 and 5, so the result of the game is 1 + 9 + (-2) = 8. 
+This is the maximal possible result that can be achieved on this board.
 
 Write a function:
 
     def solution(A)
 
-that, given a non-empty array A of N integers, returns the maximal result that can be achieved on the board represented by array A.
+that, given a non-empty array A of N integers, returns the maximal result 
+that can be achieved on the board represented by array A.
 
 For example, given the array
     A[0] = 1
@@ -43,7 +57,22 @@ the function should return 8, as explained above.
 Write an efficient algorithm for the following assumptions:
 
         N is an integer within the range [2..100,000];
-        each element of array A is an integer within the range [−10,000..10,000].
+        each element of array A is an integer within the range [-10,000..10,000].
 
 """
 
+def solution(A):
+
+    d = 6
+    n = len(A)
+    max_score = [A[0]] * d
+
+    for p in xrange(1, n):
+        max_score[p % d] = max(max_score) + A[p]
+
+    return max_score[(n - 1) % d]   
+
+A = [1, -2, 0, 9, -1, -2]
+print(solution(A))
+
+#https://codesays.com/2015/solution-to-number-solitaire-by-codility/
